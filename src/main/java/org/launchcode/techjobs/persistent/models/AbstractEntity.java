7dea2da26299472ba03d8ca3fcc5a8b7363b5811
jasminbecic <becicjasmin1@gmail.com>
2022-03-16 15:10:43 -0500

    added annotations to AbstractEntity

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -1,12 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotBlank(message = "Name cannot be blank.")
+    @Size(max = 255, message = "Name cannot be longer than 255 characters.")
     private String name;
 
     public int getId() {
